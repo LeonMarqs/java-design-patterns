a2ef3ae96fc506d6ec74f147512710c78197f937
Leonardo Marques <leomarques301@gmail.com>
2023-04-08 17:30:38 -0300

    Observer - Add Swing button action example

---
+++ src/br/com/designpattern/observer/swingExample/AngelListener.java
@@ -0,0 +1,13 @@
+package br.com.designpattern.observer.swingExample;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AngelListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Don't do it, you might regret it!");
+	}
+
+}
