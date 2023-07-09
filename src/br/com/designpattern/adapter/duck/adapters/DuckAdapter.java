7c387c137207a0c843afe17d27049b76c6cd5676
Leonardo Marques <leomarques301@gmail.com>
2023-07-09 14:50:51 -0300

    Adapter - Add DuckAdapter fly capability

--- src/br/com/designpattern/adapter/duck/adapters/DuckAdapter.java
+++ src/br/com/designpattern/adapter/duck/adapters/DuckAdapter.java
@@ -1,12 +1,21 @@
 package br.com.designpattern.adapter.duck.adapters;
 
+import java.util.Random;
+
 import br.com.designpattern.adapter.duck.Duck;
 import br.com.designpattern.adapter.duck.Turkey;
 
 public class DuckAdapter implements Turkey {
 
 	private Duck duck;
-	
+	Random rand;
+
+	public DuckAdapter(Duck duck) {
+		super();
+		this.duck = duck;
+		this.rand = new Random();
+	}
+
 	@Override
 	public void gobble() {
 		duck.fly();
@@ -14,8 +23,9 @@ public class DuckAdapter implements Turkey {
 
 	@Override
 	public void fly() {
-		duck.fly(); //TODO: see how to implement it
+		if (rand.nextInt(5) == 0) {
+			duck.fly();
+		}
 	}
 
 }
- 
